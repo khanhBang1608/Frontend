c2700b12d9704bf31961a638b643beaad83680eb
khanhBang1608 <163488048+khanhBang1608@users.noreply.github.com>
2025-04-03 23:27:25 +0700

    orderHistory

--- src/views/OrderHistoryView.vue
+++ src/views/OrderHistoryView.vue
@@ -1,13 +1,13 @@
 <script setup>
 import Header from '../components/User/Header.vue'
 import Footer from '../components/User/Footer.vue'
-import profile from '../components/User/ProFile.vue'
+import OrderHistory from '../components/User/OrderHistory.vue'
 </script>
 
 <template>
   <main>
     <Header />
-    <profile />
+    <OrderHistory />
     <Footer />
   </main>
 </template>
