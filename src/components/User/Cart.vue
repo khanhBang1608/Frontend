07cda0f88167f48e875816d16a9d6754f0ffa9d0
khanhBang1608 <163488048+khanhBang1608@users.noreply.github.com>
2025-04-19 00:28:20 +0700

    aa

--- src/components/User/Cart.vue
+++ src/components/User/Cart.vue
@@ -55,66 +55,69 @@ onMounted(() => {
   <div class="cart-container-modern">
     <h2 class="cart-title-modern">Giỏ hàng của bạn</h2>
     <div v-if="!cart || cart.length === 0" class="cart-empty-msg-modern">
-  Không có sản phẩm trong giỏ hàng
-</div>
+      Không có sản phẩm trong giỏ hàng
+    </div>
 
-<div v-else>
-  <table class="cart-table-modern">
-    <thead>
-      <tr>
-        <th>Ảnh</th>
-        <th>Sản phẩm</th>
-        <th>Size</th>
-        <th>Số lượng</th>
-        <th>Giá</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in cart" :key="item.id" class="cart-item-row-modern">
-        <td class="text-center">
-          <img
-            :src="'/images/' + item.product.imageNames[0]"
-            alt="Ảnh sản phẩm"
-            class="cart-item-img-modern"
-          />
-        </td>
-        <td class="cart-product-name-modern">{{ item.product.name }}</td>
-        <td>{{ item.productSize.sizeName }}</td>
-        <td>
-          <input
-            type="number"
-            min="1"
-            :max="item.productSize.stock"
-            class="cart-qty-input-modern"
-            v-model.number="item.quantity"
-            @change="updateQuantity(item)"
-          />
-        </td>
-        <td class="cart-price-highlight-modern">
-          {{ (item.product.price * item.quantity).toLocaleString('vi-VN') }}₫
-        </td>
-        <td class="text-center">
-          <button class="cart-remove-btn-modern" @click="handleRemoveFromCart(item.productSize.id)">
-            Xoá
-          </button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+    <div v-else>
+      <table class="cart-table-modern">
+        <thead>
+          <tr>
+            <th>Ảnh</th>
+            <th>Sản phẩm</th>
+            <th>Size</th>
+            <th>Số lượng</th>
+            <th>Giá</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in cart" :key="item.id" class="cart-item-row-modern">
+            <td class="text-center">
+              <img
+                :src="'http://localhost:8080/images/' + item.product.imageNames[0]"
+                alt="Ảnh sản phẩm"
+                class="cart-item-img-modern"
+              />
+            </td>
+            <td class="cart-product-name-modern">{{ item.product.name }}</td>
+            <td>{{ item.productSize.sizeName }}</td>
+            <td>
+              <input
+                type="number"
+                min="1"
+                :max="item.productSize.stock"
+                class="cart-qty-input-modern"
+                v-model.number="item.quantity"
+                @change="updateQuantity(item)"
+              />
+            </td>
+            <td class="cart-price-highlight-modern">
+              {{ (item.product.price * item.quantity).toLocaleString('vi-VN') }}₫
+            </td>
+            <td class="text-center">
+              <button
+                class="cart-remove-btn-modern"
+                @click="handleRemoveFromCart(item.productSize.id)"
+              >
+                Xoá
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
-  <div class="text-end cart-total-price-modern">
-    Tổng tiền:
-    {{
-      cart.reduce((total, item) => total + item.product.price * item.quantity, 0).toLocaleString('vi-VN')
-    }}₫
-  </div>
+      <div class="text-end cart-total-price-modern">
+        Tổng tiền:
+        {{
+          cart
+            .reduce((total, item) => total + item.product.price * item.quantity, 0)
+            .toLocaleString('vi-VN')
+        }}₫
+      </div>
 
-  <div class="text-end">
-    <button class="cart-clear-btn-modern" @click="clearCart">Xoá tất cả</button>
-  </div>
-</div>
-
+      <div class="text-end">
+        <button class="cart-clear-btn-modern" @click="clearCart">Xoá tất cả</button>
+      </div>
+    </div>
   </div>
 </template>
-
